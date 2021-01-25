55ac4148c132d8862dc4bb298cb17a2925c9b569
sky <mad_sky_scau@163.com>
2021-01-25 15:51:27 +0800

    小修改

--- src/server/mysql/DbUtil.java
+++ src/server/mysql/DbUtil.java
@@ -6,15 +6,15 @@ import java.sql.SQLException;
 
 public class DbUtil {
 
-    public static final String URL = "jdbc:mysql://localhost:3306/user?serverTimezone=UTC";
+    public static final String URL = "jdbc:mysql://139.199.66.139:3308/user?serverTimezone=UTC";
     public static final String USER = "root";
-    public static final String PASSWORD = "123456";
+    public static final String PASSWORD = "SCAUdachuang@database12138.";
     private static Connection conn = null;
     static{
         try {
             //1.加载驱动程序
-            Class.forName("com.mysql.cj.jdbc.Driver");
-//            Class.forName("com.mysql.jdbc.Driver");ky的版本
+            //Class.forName("com.mysql.cj.jdbc.Driver");//hl的版本
+            Class.forName("com.mysql.jdbc.Driver");//ky的版本
             //2. 获得数据库连接
             conn = DriverManager.getConnection(URL, USER, PASSWORD);
         } catch (ClassNotFoundException | SQLException e) {
